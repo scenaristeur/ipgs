a032090d7f108adcb0ba6b722e7fde02bd50df96
Scenaristeur <scenrister@galcm>
2021-03-02 19:39:37 +0100

    chrome network imgs

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ipgs/precache-manifest.a9bbaf4375cddfbaa935bd4478866aef.js"
+  "/ipgs/precache-manifest.23c4988c186769f3faabd577fda8dffd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Ipgs"});
