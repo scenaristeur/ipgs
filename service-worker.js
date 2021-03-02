98ef10107c9dc9ee8a3920112e46dfb10628c6fd
Scenaristeur <>
2021-03-02 01:38:10 +0100

    refactor network

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ipgs/precache-manifest.b0d3ab2f606db996e016c4f214714983.js"
+  "/ipgs/precache-manifest.dadeb803b8e898e3169eb56cd846cedd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Ipgs"});
