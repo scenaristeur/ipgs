66010f6892cdfc00db42e45554dc894218979415
Scenaristeur <>
2021-03-02 12:41:40 +0100

    restore network css

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ipgs/precache-manifest.4b7526eedfda245a8dc1122b8e530855.js"
+  "/ipgs/precache-manifest.f82836f9b6109cf220c503565c88622c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Ipgs"});
