90e70201d48b277a9bea44b42442a30245fb459b
Scenaristeur <scenrister@galcm>
2021-03-27 15:35:14 +0100

    read file ttl

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ipgs/precache-manifest.69d471bc39f6a9b0908af5abd09ad37e.js"
+  "/ipgs/precache-manifest.9b4f7cce874e62bbe1f7b7b01f139ed3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Ipgs"});
